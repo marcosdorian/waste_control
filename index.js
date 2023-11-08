4fd350ba5f50f447ffcba0992951d79cd0db73f0
marcosdorian <marcosdorian@gmail.com>
2023-11-08 19:08:20 -0300

    error messages

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-function validateFields() {
-    const emailValid = isEmailValid();
-    document.getElementById("recover-password-button").disabled = !emailValid;
+// these two next functions are called in the html code.
 
-    const passwordValid = isPasswordValid();
-    document.getElementById("login-button").disabled = !emailValid || !passwordValid;
+function onChangeEmail() {
+    toggleButtonDisabled();
+    toggleEmailErrors();
+}
+
+function onChangePassword() {
+    toggleButtonDisabled();
+    togglePasswordError();
 }
 
 function isEmailValid() {
@@ -12,6 +16,38 @@ function isEmailValid() {
         return false;
     }
     return validateEmail(email);
+}
+
+function toggleEmailErrors() {
+    const email = document.getElementById("email").value;
+    if (!email) {
+        document.getElementById("email-required-error").style.display = "block";
+    } else {
+        document.getElementById("email-required-error").style.display = "none";
+    }
+
+    if (!validateEmail(email)) {
+        document.getElementById("email-invalid-error").style.display = "block";
+    } else {
+        document.getElementById("email-invalid-error").style.display = "none";
+    }
+}
+
+function togglePasswordError() {
+    const password = document.getElementById("password").value;
+    if (!password) {
+        document.getElementById("password-required-error").style.display = "block";
+    } else {
+        document.getElementById("password-required-error").style.display = "none";
+    }
+}
+
+function toggleButtonDisabled() {
+    const emailValid = isEmailValid();
+    document.getElementById("recover-password-button").disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById("login-button").disabled = !emailValid || !passwordValid;
 }
 
 function isPasswordValid() {
